85a02830fa357d419a7aaa6053a85bf748d79a69
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-03-03 23:59:30 +0100

    For some reason router.use() does not work

--- ROUTER/PaymentRouter.js
+++ ROUTER/PaymentRouter.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const AdminService = require('../CONTROLLER/AdminService')
 const {protect, auth} = require('../MIDDLEWARES/protect')
 
- router.use(auth('Admin'))
+// router.use(auth('Admin'))
 
-router.get("/", protect, AdminService.getUsers)
+router.get("/", protect, auth('Admin'), AdminService.getUsers)
 router.get("/:userId", protect, AdminService.getSingleUser)
 router.post("/", protect, AdminService.makeDeposit)
 router.get('/deposits', protect, AdminService.getDeposits)
