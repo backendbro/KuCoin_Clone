9c8db4a52abab1b9b2e0f619db01d29084081119
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-03-03 23:49:15 +0100

    Added the protect middleware back

--- ROUTER/PaymentRouter.js
+++ ROUTER/PaymentRouter.js
@@ -4,10 +4,10 @@ const {protect, auth} = require('../MIDDLEWARES/protect')
 
  router.use(auth('Admin'))
 
-router.get("/", AdminService.getUsers)
-router.get("/:userId", AdminService.getSingleUser)
-router.post("/", AdminService.makeDeposit)
-router.get('/deposits', AdminService.getDeposits)
-router.get('/deposits/:id', AdminService.getSingleDeposits)
+router.get("/", protect, AdminService.getUsers)
+router.get("/:userId", protect, AdminService.getSingleUser)
+router.post("/", protect, AdminService.makeDeposit)
+router.get('/deposits', protect, AdminService.getDeposits)
+router.get('/deposits/:id', protect, AdminService.getSingleDeposits)
 
 module.exports = router 
